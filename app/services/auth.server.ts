a14b5871849a43bab7bfbe174fa16d7e64dc8619
laurynas <unextbusinessman@gmail.com>
2025-06-05 10:42:48 +0200

    Refactor authentication service to remove unused plugins and simplify configuration. Retain passkey plugin for enhanced security.

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,18 +1,15 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { admin, emailOTP } from 'better-auth/plugins';
+import { passkey } from 'better-auth/plugins/passkey';
+
 import { db } from '~/db';
 import { env } from './env.server';
-import { passkey } from 'better-auth/plugins/passkey';
 export { APIError } from 'better-auth/api';
 
 export const auth = betterAuth({
   appName: 'BetterAuth',
   baseURL: env.BETTER_AUTH_URL,
   secret: env.BETTER_AUTH_SECRET,
-  rateLimit: {
-    enabled: false
-  },
   database: drizzleAdapter(db, {
     provider: 'sqlite'
   }),
@@ -23,31 +20,7 @@ export const auth = betterAuth({
       return Promise.resolve();
     }
   },
-  plugins: [
-    admin({
-      adminUserIds: ['Vt7dTS6mEnUsQpg2G4lLomYdeGfrEQqI'],
-      // Allow any user to stop impersonating (including impersonated users)
-      permissions: {
-        stopImpersonating: '*' // This allows any user to stop impersonation
-      }
-
-      // impersonateUser: async ({ userId }: { userId: string }) => {
-      //   console.log('Impersonating user:', userId);
-      //   return Promise.resolve();
-      // }
-    }),
-    passkey(),
-    emailOTP({
-      async sendVerificationOTP({ email, otp, type }) {
-        console.log(`Sending ${type} OTP to ${email}: ${otp}`);
-        return Promise.resolve();
-      },
-      sendVerificationOnSignUp: true,
-      otpLength: 6,
-      expiresIn: 600000,
-      disableSignUp: false
-    })
-  ]
+  plugins: [passkey()]
 });
 
 export const isLoggedIn = async ({ request }: { request: Request }) => {
